71d274e58ccf6e6aff6d070e38bfc7a1494f92eb
Xochitl Menjivar <xandra.menji@gmail.com>
2018-01-16 22:03:18 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,6 +7,6 @@ function updatePlaylist(playlist, artistName, songTitle){
   return playlist;
 }
 
-function removeFromPlaylist(playlist, artistame){
+function removeFromPlaylist(playlist, artistName){
   delete playlist.artistName;
 }
