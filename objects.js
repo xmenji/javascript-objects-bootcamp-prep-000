994255e5ee4350d632390572521eadb2afa5debc
Xochitl Menjivar <xandra.menji@gmail.com>
2018-01-16 21:58:57 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,6 +3,5 @@ var playlist = {
 };
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist = {artistName : songTitle};
-  return playlist;
+ return Object.assign({}, playlist, {artistName : songTitle});
 }
